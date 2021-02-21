df14ad63fe3358954bffdf745571015a48b5cbd1
Junarsan <apolinar.magtanong@gmail.com>
2021-02-21 11:17:26 +1100

    Added gitignore folder

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Schema for burgers_db 
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create database department
+CREATE DATABASE burgers_db;
+
+-- Use created database to create table
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INTEGER AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN,
+    PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Champ', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Chicken Deluxe', TRUE);
