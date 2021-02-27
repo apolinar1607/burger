127d2bc25ad8f63aa7777276da1f8d6975fd406a
Junarsan <apolinar.magtanong@gmail.com>
2021-02-27 14:15:16 +1100

    Modified connection.js to address connection issue with the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 -- Schema for burgers_db 
-DROP DATABASE IF EXISTS burgers_db;
+-- DROP DATABASE IF EXISTS burgers_db;
 
 -- Create database department
-CREATE DATABASE burgers_db;
+-- CREATE DATABASE burgers_db;
 
 -- Use created database to create table
-USE burgers_db;
+-- USE burgers_db;
 
 CREATE TABLE burgers (
 	id INTEGER AUTO_INCREMENT NOT NULL,
